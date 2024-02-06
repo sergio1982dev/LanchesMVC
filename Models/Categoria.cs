43ce092fb991afc54edc8ee51e7c2f5b0a9f4192
sergio1982dev <sergio1982.dev@gmail.com>
2024-02-06 20:06:41 -0300

    criados a partial view carousel, entidades de dominio e classe de contexto, configurado o EFcore, utilizado data annotation e realizado migration para geracao das tabelas

---
+++ Models/Categoria.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LanchesMVC.Models
+{
+    public class Categoria
+    {
+        public int CategoriaId { get; set; }
+
+        [Required(ErrorMessage ="Campo obrigatório")]
+        [StringLength(50,ErrorMessage ="Máximo de 50 caracteres")]
+        public string? CategoriaNome { get; set; }
+
+        [Required(ErrorMessage ="Campo obrigatório")]
+        [StringLength(200, ErrorMessage ="Máximo 200 caracteres")]
+        public string? Descricao { get; set; }
+
+        public List<Lanche>? Lanches { get; set; }
+    }
+}
---
+++ Models/Lanche.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LanchesMVC.Models
+{
+    public class Lanche
+    {
+        public int LancheId { get; set; }
+
+        [Required(ErrorMessage = "Campo obrigatório")]
+        [StringLength(80,MinimumLength=5, ErrorMessage ="Mínimo 5 e máximo 80 caracteres")]
+        public string? Nome { get; set; }
+
+        [Required(ErrorMessage = "Campo obrigatório")]
+        [StringLength(100, ErrorMessage = "Máximo 100 caracteres")]
+        public string? DescricaoCurta { get; set; }
+
+        [Required(ErrorMessage = "Campo obrigatório")]
+        [StringLength(200, ErrorMessage = "Máximo 200 caracteres")]
+        public string? DescricaoDetalhada { get; set; }
+
+        [Required]
+        [Column(TypeName ="decimal(10, 2)")]
+        public decimal Preco { get; set; }
+
+        [StringLength(200, ErrorMessage ="Máximo 200 caracteres")]
+        public string? ImagemUrl { get; set; }
+
+        [StringLength(200, ErrorMessage = "Máximo 200 caracteres")]
+        public string? ImagemThumbnailUrl { get; set; }
+        public bool IsLanchePreferido { get; set; }
+        public bool EmEstoque { get; set; }
+
+        public int CategoriaId { get; set; }
+        public virtual Categoria? Categoria { get; set; }
+    }
+}
